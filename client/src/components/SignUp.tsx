fae52638b808457ed34b1af920db77fcb6021ff0
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-03-20 12:54:53 -0400

    Added responsiveness to sign up page, for sm plus screens

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -16,14 +16,14 @@ async function auth() {
 function SignUp() {
   return (
     
-    <div className="w-full h-screen p-4 dark:bg-gray-800 flex flex-col items-center">
-        <div className="h-1/2 flex items-center justify-center">
+    <div className="w-full h-screen p-4 sm:p-8 dark:bg-gray-800 flex flex-col items-center">
+        <div className="h-1/2 sm:h-3/5 flex items-center justify-center">
             <img src={travelImg} />
         </div>
-        <h4 className="mt-8 text-3xl font-medium text-gray-900 dark:text-white text-center">Collaborate Confidently!</h4>
-        <h5 className="mt-8 text-xl font-medium text-gray-900 dark:text-white text-center">Use <span className=" text-yellow-300">Collabify</span> to connect with contributors, even on the go</h5>
+        <h4 className="mt-8 sm:mt-16 text-3xl sm:text-4xl font-medium text-gray-900 dark:text-white text-center">Collaborate Confidently!</h4>
+        <h5 className="mt-8 text-xl sm:text-2xl font-medium text-gray-900 dark:text-white text-center">Use <span className=" text-yellow-300">Collabify</span> to connect with contributors, even on the go</h5>
         
-        <button className="w-full mt-12 px-4 py-2 border gap-2 border-slate-200 flex justify-center items-center text-sm dark:border-slate-700 rounded-full text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+        <button className="w-full sm:w-3/5 mt-12 px-4 py-2 border gap-2 border-slate-200 flex justify-center items-center text-sm dark:border-slate-700 rounded-full text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
                 onClick={() => auth()}>
             <img className="w-5 h-5" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google-logo" />
             <span>Continue with Google</span>
